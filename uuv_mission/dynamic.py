1e2eea79e958803e92a0d6407f1a7c194a187745
Naturaeplants <arj.puvana@gmail.com>
2024-10-20 17:11:35 +0100

    Finalizing PD controller implementation

--- uuv_mission/dynamic.py
+++ uuv_mission/dynamic.py
@@ -7,43 +7,38 @@ from .terrain import generate_reference_and_limits
 
 class Submarine:
     def __init__(self):
-
         self.mass = 1
         self.drag = 0.1
         self.actuator_gain = 1
-
-        self.dt = 1 # Time step for discrete time simulation
-
+        self.dt = 1  # Time step for discrete time simulation
         self.pos_x = 0
         self.pos_y = 0
-        self.vel_x = 1 # Constant velocity in x direction
+        self.vel_x = 1  # Constant velocity in x direction
         self.vel_y = 0
-
 
     def transition(self, action: float, disturbance: float):
         self.pos_x += self.vel_x * self.dt
         self.pos_y += self.vel_y * self.dt
-
         force_y = -self.drag * self.vel_y + self.actuator_gain * (action + disturbance)
         acc_y = force_y / self.mass
         self.vel_y += acc_y * self.dt
 
     def get_depth(self) -> float:
         return self.pos_y
-    
+
     def get_position(self) -> tuple:
         return self.pos_x, self.pos_y
-    
+
     def reset_state(self):
         self.pos_x = 0
         self.pos_y = 0
         self.vel_x = 1
         self.vel_y = 0
-    
+
 class Trajectory:
     def __init__(self, position: np.ndarray):
-        self.position = position  
-        
+        self.position = position
+
     def plot(self):
         plt.plot(self.position[:, 0], self.position[:, 1])
         plt.show()
@@ -52,12 +47,9 @@ class Trajectory:
         x_values = np.arange(len(mission.reference))
         min_depth = np.min(mission.cave_depth)
         max_height = np.max(mission.cave_height)
-
         plt.fill_between(x_values, mission.cave_height, mission.cave_depth, color='blue', alpha=0.3)
-        plt.fill_between(x_values, mission.cave_depth, min_depth*np.ones(len(x_values)), 
-                         color='saddlebrown', alpha=0.3)
-        plt.fill_between(x_values, max_height*np.ones(len(x_values)), mission.cave_height, 
-                         color='saddlebrown', alpha=0.3)
+        plt.fill_between(x_values, mission.cave_depth, min_depth * np.ones(len(x_values)), color='saddlebrown', alpha=0.3)
+        plt.fill_between(x_values, max_height * np.ones(len(x_values)), mission.cave_height, color='saddlebrown', alpha=0.3)
         plt.plot(self.position[:, 0], self.position[:, 1], label='Trajectory')
         plt.plot(mission.reference, 'r', linestyle='--', label='Reference')
         plt.legend(loc='upper right')
@@ -71,7 +63,7 @@ class Mission:
 
     @classmethod
     def random_mission(cls, duration: int, scale: float):
-        (reference, cave_height, cave_depth) = generate_reference_and_limits(duration, scale)
+        reference, cave_height, cave_depth = generate_reference_and_limits(duration, scale)
         return cls(reference, cave_height, cave_depth)
 
     @classmethod
@@ -82,30 +74,43 @@ class Mission:
         cave_depth = data['cave_depth'].to_numpy()
         return cls(reference, cave_height, cave_depth)
 
+# New controller module
+class PDController:
+    def __init__(self, kp=0.15, kd=0.6):
+        self.kp = kp
+        self.kd = kd
+        self.prev_error = 0
+    
+    def control(self, error):
+        derivative = error - self.prev_error
+        u_t = self.kp * error + self.kd * derivative
+        self.prev_error = error
+        return u_t
 
 class ClosedLoop:
     def __init__(self, plant: Submarine, controller):
         self.plant = plant
         self.controller = controller
 
-    def simulate(self,  mission: Mission, disturbances: np.ndarray) -> Trajectory:
-
+    def simulate(self, mission: Mission, disturbances: np.ndarray) -> Trajectory:
         T = len(mission.reference)
         if len(disturbances) < T:
             raise ValueError("Disturbances must be at least as long as mission duration")
         
         positions = np.zeros((T, 2))
         actions = np.zeros(T)
+        errors = np.zeros(T)
         self.plant.reset_state()
 
-        for t in range(T):
+        for t in range(1, T):
             positions[t] = self.plant.get_position()
             observation_t = self.plant.get_depth()
-            # Call your controller here
+            errors[t] = mission.reference[t] - observation_t
+            actions[t] = self.controller.control(errors[t])
             self.plant.transition(actions[t], disturbances[t])
-
-        return Trajectory(positions)
         
+        return Trajectory(positions)
+
     def simulate_with_random_disturbances(self, mission: Mission, variance: float = 0.5) -> Trajectory:
         disturbances = np.random.normal(0, variance, len(mission.reference))
         return self.simulate(mission, disturbances)
